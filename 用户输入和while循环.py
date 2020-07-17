87033c052ba128164d0efc94bb8c8eb1f8917592
laidadada <382135230@qq.com>
2020-07-17 15:16:52 +0800

    修改了内容

--- 用户输入和while循环.py
+++ 用户输入和while循环.py
@@ -20,28 +20,28 @@
 #     print(current_number)
 #     current_number += 1
 
-# prompt = "\nTell me something, and I will repeat it back to you:"
-# prompt += "\nEnter 'quit' to end the program. "
-#
-# active = True
-# while active:
-#     message = input(prompt)
-#     if message == "quit":
-#         active = False
-#     else:
-#         print(message)
-#
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
 
-# prompt = "\nPlease enter the name of a city you have visited:"
-# prompt += "\n(Enter 'quit' when you are finished.) "
-#
-# while True:
-#     city = input(prompt)
-#
-#     if city == 'quit':
-#         break
-#     else:
-#         print("I want to go " + city.title())
+active = True
+while active:
+    message = input(prompt)
+    if message == "quit":
+        active = False
+    else:
+        print(message)
+
+
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I want to go " + city.title())
 #
 
 current_number = 0
