1cad6f80bdece86f46921326384d8d1100691192
laidadada <382135230@qq.com>
2020-07-29 17:30:18 +0800

    20200729 --132页

--- 用户输入和while循环.py
+++ 用户输入和while循环.py
@@ -20,37 +20,57 @@
 #     print(current_number)
 #     current_number += 1
 
-prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
-
-active = True
-while active:
-    message = input(prompt)
-    if message == "quit":
-        active = False
-    else:
-        print(message)
-
-
-prompt = "\nPlease enter the name of a city you have visited:"
-prompt += "\n(Enter 'quit' when you are finished.) "
-
-while True:
-    city = input(prompt)
-
-    if city == 'quit':
-        break
-    else:
-        print("I want to go " + city.title())
+# prompt = "\nTell me something, and I will repeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program. "
 #
+# active = True
+# while active:
+#     message = input(prompt)
+#     if message == "quit":
+#         active = False
+#     else:
+#         print(message)
+#
+#
+# prompt = "\nPlease enter the name of a city you have visited:"
+# prompt += "\n(Enter 'quit' when you are finished.) "
+#
+# while True:
+#     city = input(prompt)
+#
+#     if city == 'quit':
+#         break
+#     else:
+#         print("I want to go " + city.title())
+# #
+#
+# current_number = 0
+# while current_number <= 30:
+#     current_number += 1
+#
+#     if current_number % 2 == 0:
+#         continue
+#     print(current_number)
 
-current_number = 0
-while current_number <= 30:
-    current_number += 1
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
 
-    if current_number % 2 == 0:
-        continue
-    print(current_number)
+print(pets)
 
 
---128
+responses = {}
+responses_active = True
+while responses_active:
+    name =input("\nWhat is you name?")
+    response = input("Which mountain would you like to climb someday ?")
+    responses[name] = response
+    repeat = input("\nWould you like to let another person respond?(yes/no)")
+    if repeat == 'no':
+        responses_active = False
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+ print(name + " would like to climb " + response + ".")
+
+
---
+++ 练习.py
@@ -0,0 +1,28 @@
+# sandwich_orders = ['hanbao','kele','shutiao']
+# finished_sandwiches = []
+#
+# while sandwich_orders:
+#
+#     sandwich_order = sandwich_orders.pop()
+#     print("Show to list:" + sandwich_order.title())
+#     finished_sandwiches.append(sandwich_order)
+#
+# print("\nI made your tuna sandwich:" )
+# for finished_sandwiche in finished_sandwiches:
+#     print(finished_sandwiche.title())
+#
+
+sandwich_orders = ['pastrami','hanbao','pastrami','kele','pastrami','shutiao']
+finished_sandwiches = []
+print("Five cigarettes of bacon are sold out")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+print(sandwich_orders)
+while sandwich_orders:
+    sandwich_order = sandwich_orders.pop()
+    finished_sandwiches.append(sandwich_order)
+print("\nWhat's left:")
+for finished_sandwiche in finished_sandwiches:
+    print(finished_sandwiche.title())
+
+
